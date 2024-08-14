08f400f605abb5126072014e2816fe80cdc227ae
YY-312 <2201786G@student.tp.edu.sg>
2024-08-14 12:40:16 +0800

    Update apache_https.py

--- apache_https.py
+++ apache_https.py
@@ -1,20 +1,21 @@
 import subprocess
 
-def run_command(command):
-    """Function to run shell commands."""
-    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+def run_command_in_container(container_name, command):
+    """Function to run shell commands inside a Docker container."""
+    full_command = f"docker exec -it {container_name} bash -c \"{command}\""
+    result = subprocess.run(full_command, shell=True, capture_output=True, text=True)
     if result.returncode == 0:
-        print(f"Command '{command}' executed successfully.")
+        print(f"Command '{command}' executed successfully in container '{container_name}'.")
     else:
-        print(f"Error executing '{command}': {result.stderr}")
+        print(f"Error executing '{command}' in container '{container_name}': {result.stderr}")
         raise Exception(result.stderr)
 
-def configure_ssl_on_apache():
+def configure_ssl_on_apache(container_name):
     # Rename and move SSL certificate
-    run_command("cp /usr/local/apache2/conf/ssl/apache.crt /usr/local/apache2/conf/server.crt")
+    run_command_in_container(container_name, "cp /usr/local/apache2/conf/ssl/apache.crt /usr/local/apache2/conf/server.crt")
 
     # Rename and move SSL key
-    run_command("cp /usr/local/apache2/conf/ssl/apache.key /usr/local/apache2/conf/server.key")
+    run_command_in_container(container_name, "cp /usr/local/apache2/conf/ssl/apache.key /usr/local/apache2/conf/server.key")
 
     # Update httpd-ssl.conf with SSL settings
     ssl_conf_content = """
@@ -36,20 +37,17 @@ ServerName 192.168.2.2
     </VirtualHost>
 </IfModule>
 """
-    with open("/usr/local/apache2/conf/extra/httpd-ssl.conf", "w") as ssl_conf_file:
-        ssl_conf_file.write(ssl_conf_content)
+    run_command_in_container(container_name, f"echo '{ssl_conf_content}' > /usr/local/apache2/conf/extra/httpd-ssl.conf")
 
     # Include httpd-ssl.conf in main configuration
-    with open("/usr/local/apache2/conf/httpd.conf", "a") as httpd_conf_file:
-        httpd_conf_file.write("\nInclude conf/extra/httpd-ssl.conf\n")
+    run_command_in_container(container_name, "echo 'Include conf/extra/httpd-ssl.conf' >> /usr/local/apache2/conf/httpd.conf")
 
     # Load SSL and socache_shmcb modules
-    with open("/usr/local/apache2/conf/httpd.conf", "a") as httpd_conf_file:
-        httpd_conf_file.write("\nLoadModule ssl_module modules/mod_ssl.so\n")
-        httpd_conf_file.write("LoadModule socache_shmcb_module modules/mod_socache_shmcb.so\n")
+    run_command_in_container(container_name, "echo 'LoadModule ssl_module modules/mod_ssl.so' >> /usr/local/apache2/conf/httpd.conf")
+    run_command_in_container(container_name, "echo 'LoadModule socache_shmcb_module modules/mod_socache_shmcb.so' >> /usr/local/apache2/conf/httpd.conf")
 
     # Restart Apache to apply changes
-    run_command("apachectl restart")
+    run_command_in_container(container_name, "apachectl restart")
 
 if __name__ == "__main__":
-    configure_ssl_on_apache()
+    configure_ssl_on_apache("clab-firstlab-apache-server")
